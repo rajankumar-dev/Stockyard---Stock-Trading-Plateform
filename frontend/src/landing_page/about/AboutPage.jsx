13b13161a4685da24aa2fa027f4a9f9c5e4c1d56
rajankumar-dev <rk1620703@gmail.com>
2025-11-16 15:52:02 +0530

    complete about component

---
+++ frontend/src/landing_page/about/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./Hero";
+import Team from "./Team";
+
+function Education(){
+    return (
+        <>
+            <Hero/>
+            <Team/>
+        </>
+    );
+}
+
+export default Education;
